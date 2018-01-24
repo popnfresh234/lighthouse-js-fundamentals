05dbba925df99859e5914e6ec2974d74492a3c2d
Alex Holliday <ajhollid@gmail.com>
2018-01-24 04:47:56 +0000

    adds conditionals exercises

---
+++ icecream.js
@@ -0,0 +1,6 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rcky road", "strawberry"];
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+console.log(iceCreamFlavours.length);

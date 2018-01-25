543ac7469d4cb636fed71b85096dcd18e97d6615
Alex Holliday <ajhollid@gmail.com>
2018-01-25 15:27:10 +0000

    Adds looping practice

---
+++ ingredients.js
@@ -0,0 +1,22 @@
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// var count = 0;
+// while(count < ingredients.length){
+//   console.log(ingredients[count]);
+//   count ++;
+// }
+
+// for (var i = 0; i < ingredients.length; i++){
+//   console.log(ingredients[i]);
+// }
+
+// for (var i = ingredients.length - 1; i >= 0; i--){
+//   console.log(ingredients[i]);
+// }
+
+var count = ingredients.length -1
+while (count >= 0){
+  console.log(ingredients[count]);
+  count --;
+}

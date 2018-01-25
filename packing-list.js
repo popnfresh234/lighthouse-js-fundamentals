6fee04668b04ff4d33083ff9bc9b8eeffcac4a07
Alex Holliday <ajhollid@gmail.com>
2018-01-25 15:19:55 +0000

    Adds looping exercises

---
+++ packing-list.js
@@ -0,0 +1,4 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+for (var i = 0; i < packingList.length; i++){
+  console.log(packingList[i]);
+}
